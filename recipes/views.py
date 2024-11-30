4708f0dfad7bd5786f54cb9223c5968527f3d937
marcuszou <marcus.zou@gmail.com>
2024-11-30 13:43:24 -0700

    5.4

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from recipes.models import Recipe
+
 # Create your views here.
 def recipes(request):
+    # recipes = Recipe.objects.filter(category__name__iexact="salad")
+    # recipes = Recipe.objects.exclude(name__contains="Chocolate")
+    recipes = Recipe.objects.filter(category__name__iexact="Soup").exclude(name__contains="chocolate").order_by("-date_added")
+    print("Recipes", recipes)
     return HttpResponse("Hello from Recipes")
